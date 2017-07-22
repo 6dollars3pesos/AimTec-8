c6fe24995e662e13240d3a28bcbbcc9fed447b31
chidoriii <satyam.pramod.mohan@gmail.com>
2017-07-22 23:04:35 +0200

    Added Auto Leveler

---
+++ Auto Leveler/MenuManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aimtec;
+using Aimtec.SDK.Menu;
+using Aimtec.SDK.Menu.Components;
+using Aimtec.SDK.Orbwalking;
+using Aimtec.SDK.Util;
+
+namespace Auto_Leveler {
+
+    class MenuManager {
+
+        private static readonly Menu Menu = new Menu("autoLeveler", "Auto Leveler", true);
+
+        public static Dictionary<SpellSlot, int> Spells = new Dictionary<SpellSlot, int>();
+
+        public static void Create() {
+            new Orbwalker().Attach(Menu);
+
+            Menu levels = new Menu("levels", "Levels") {
+                new MenuSlider("q", "Q", 1, 1, 4),
+                new MenuSlider("w", "W", 2, 1, 4),
+                new MenuSlider("e", "E", 3, 1, 4),
+                new MenuSlider("r", "R", 4, 1, 4)
+            };
+
+            levels.OnValueChanged += LevelsOnOnValueChanged;
+
+            Menu.Add(levels);
+
+            Menu.Attach();
+
+            ReadSpellValues();
+        }
+
+        private static void ReadSpellValues() {
+            Spells[SpellSlot.Q] = Menu["levels"]["q"].Value;
+            Spells[SpellSlot.W] = Menu["levels"]["w"].Value;
+            Spells[SpellSlot.E] = Menu["levels"]["e"].Value;
+            Spells[SpellSlot.R] = Menu["levels"]["r"].Value;
+        }
+
+        private static void LevelsOnOnValueChanged(MenuComponent sender, ValueChangedArgs args) {
+            if (HasValidPriorities()) {
+                ReadSpellValues();
+            }
+            else {
+                Console.WriteLine("Invalid spell priorities!");
+            }
+        }
+
+        private static bool HasValidPriorities() {
+            List<int> priorities = new List<int>(new int[] {
+                Menu["levels"]["q"].Value, Menu["levels"]["w"].Value,
+                Menu["levels"]["e"].Value, Menu["levels"]["r"].Value
+            });
+
+            foreach (int priority in priorities) {
+                if (priorities.FindAll(x => x == priority).Count > 1) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+
+}
---
+++ Auto Leveler/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aimtec;
+using Aimtec.SDK.Events;
+
+namespace Auto_Leveler {
+
+    public class Program {
+
+        private static Obj_AI_Hero Player;
+
+        public static void Main(string[] args) {
+            GameEvents.GameStart += GameEvents_GameStart;
+        }
+
+        private static void GameEvents_GameStart() {
+            MenuManager.Create();
+
+            Player = ObjectManager.GetLocalPlayer();
+
+            Obj_AI_Base.OnLevelUp += ObjAiBaseOnOnLevelUp;
+        }
+
+        private static void ObjAiBaseOnOnLevelUp(Obj_AI_Base objAi, Obj_AI_BaseLevelUpEventArgs e) {
+            if (objAi.GetType() == typeof(Obj_AI_Hero)) {
+                Obj_AI_Hero hero = (Obj_AI_Hero) objAi;
+
+                List<KeyValuePair<SpellSlot, int>> pairs = MenuManager.Spells.ToList();
+                pairs.Sort((p1, p2) => p1.Value.CompareTo(p2.Value));
+
+                foreach (KeyValuePair<SpellSlot, int> pair in pairs) {
+                    Spell spell = hero.SpellBook.GetSpell(pair.Key);
+                    int maxLevel;
+
+                    if (pair.Key.Equals(SpellSlot.R)) {
+                        maxLevel = 3;
+                    }
+                    else {
+                        maxLevel = 5;
+                    }
+
+                    if (spell.Level < maxLevel) {
+                        hero.SpellBook.LevelSpell(pair.Key);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
